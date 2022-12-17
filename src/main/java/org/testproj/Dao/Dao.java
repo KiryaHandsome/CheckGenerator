f6ef4c60df9c22fa90af2f3238fa4eb8ef021b7a
Kiryl Pryhozhy <kirill19962003@gmail.com>
2022-12-17 15:17:42 +0300

    add dao interface

---
+++ src/main/java/org/testproj/Dao/Dao.java
@@ -0,0 +1,17 @@
+package org.testproj.Dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+    Optional<T> get(long id);
+
+    List<T> getAll();
+
+    void save(T obj);
+
+    void update(T obj, String[] params);
+
+    void delete(T t);
+}
