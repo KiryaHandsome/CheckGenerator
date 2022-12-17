b553461091de3dc9f9bb8a36fca44b6ba5343c45
Kiryl Pryhozhy <kirill19962003@gmail.com>
2022-12-17 15:17:07 +0300

    implement Builder pattern for Product

--- src/main/java/org/testproj/CheckRunner.java
+++ src/main/java/org/testproj/CheckRunner.java
@@ -1,7 +1,19 @@
 package org.testproj;
 
+import org.testproj.Entities.Product;
+
 public class CheckRunner {
+    public static String[] names = new String[]{"Milk", "Beans", "Sausages"};
+    public static double[] prices = new double[]{18.77, 15.19, 99.99};
+
     public static void main(String[] args) {
+
+        Product product = new Product().getBuilder()
+                .setId(1)
+                .setName("Kirya")
+                .setPrice(999.85)
+                .build();
+        System.out.println(product);
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/testproj/Entities/Product.java
+++ src/main/java/org/testproj/Entities/Product.java
@@ -1,37 +1,91 @@
 package org.testproj.Entities;
 
+import java.util.Date;
+
 public class Product {
     private long id;
     private String name;
     private double price;
 
-    public Product(long id, String name, double price) {
+    //TODO: add weight and expiration date to database and builder
+    private double weight;
+    private Date expirationDate;
+
+    public Product() {}
+
+    public Product(long id, String name, double price, double weight, Date expirationDate) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.weight = weight;
+        this.expirationDate = expirationDate;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
     }
 
     public long getId() {
         return id;
     }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public ProductBuilder getBuilder() {
+        return new ProductBuilder();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public static class ProductBuilder {
+        private long id;
+        private String name;
+        private double price;
+        private double weight;
+        private Date expirationDate;
+
+        public ProductBuilder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public void setWeight(double weight) {
+            this.weight = weight;
+        }
+
+        public void setExpirationDate(Date expirationDate) {
+            this.expirationDate = expirationDate;
+        }
+
+        public ProductBuilder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(id, name, price,
+                    weight, expirationDate);
+        }
     }
 }
