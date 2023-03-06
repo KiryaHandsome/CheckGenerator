866bf29e03ab9d529b17c4ded03b7757ff179745
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-03-06 18:25:28 +0300

    feat: add cache interface and two impl classes

---
+++ src/main/java/ru/clevertec/cache/impl/LRUCache.java
@@ -0,0 +1,25 @@
+package ru.clevertec.cache.impl;
+
+import ru.clevertec.cache.Cache;
+
+public class LRUCache<T> implements Cache<T> {
+    @Override
+    public T get(int id) {
+        return null;
+    }
+
+    @Override
+    public void put(int id, T value) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void post(int id, T value) {
+
+    }
+}
