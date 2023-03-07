aac2e3a9288cfbf5afccc5b9c2a3471ea8317de6
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-03-07 11:33:26 +0300

    feat: implement LRU cache

--- src/main/java/ru/clevertec/cache/impl/LRUCache.java
+++ src/main/java/ru/clevertec/cache/impl/LRUCache.java
@@ -1,25 +1,41 @@
 package ru.clevertec.cache.impl;
 
-import ru.clevertec.cache.Cache;
+import ru.clevertec.cache.CacheManager;
 
-public class LRUCache<T> implements Cache<T> {
-    @Override
-    public T get(int id) {
-        return null;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<T> implements CacheManager<T> {
+    private LinkedHashMap<Integer, T> cache;
+    private final int CAPACITY;
+
+    public LRUCache(int capacity) {
+        this.CAPACITY = capacity;
+        cache = new LinkedHashMap<>(CAPACITY, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
     }
 
     @Override
-    public void put(int id, T value) {
+    public T get(int id) {
+        return cache.getOrDefault(id, null);
+    }
 
+    @Override
+    public boolean contains(int id) {
+        return cache.containsKey(id);
+    }
+
+    @Override
+    public void put(int id, T object) {
+        cache.put(id, object);
     }
 
     @Override
     public void delete(int id) {
-
-    }
-
-    @Override
-    public void post(int id, T value) {
-
+        cache.remove(id);
     }
 }
